44304d787465182f88171c78bd719e47fb081452
BOMNguyen <tuannguyen@vinaresearch.net>
2016-12-12 14:43:00 +0700

    [Upload]

---
+++ common/js/init.js
@@ -0,0 +1,9 @@
+(function($) {
+    $(document).on("click","a.group-name",function(){
+      $(this).next("ul[data-group-name]").stop(true,false).slideToggle();
+    })
+
+    $(document).on("click","a.menu-btn",function(){
+      $(this).next("nav").stop(true,false).slideToggle();
+    })
+})(jQuery);
