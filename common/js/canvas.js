508d29840e216a23a1c6d6fc9048325359ad8ee1
BOMNguyen <tuannguyen@vinaresearch.net>
2016-12-12 15:11:11 +0700

    [UPLOAD]

--- common/js/canvas.js
+++ common/js/canvas.js
@@ -35,7 +35,7 @@ Background.prototype = {
 /*
  * Item class
  */
-function Item(src, x, y, w, h, drag) {
+function Item(src, x, y, w, h) {
     this.src = src || '';
     this.x = x || 0;
     this.y = y || 0;
@@ -45,51 +45,66 @@ function Item(src, x, y, w, h, drag) {
 
 Item.prototype = {
     drag: false,
+    flip: false,
 
     _draw: function(context) {
         let image = new Image();
         image.src = this.src;
-        context.drawImage(image, this.x, this.y, this.w, this.h);
+
+        if(this.flip) {
+            context.save();
+            context.scale(-1, 1);
+            context.drawImage(image, -(this.x + this.w), this.y, this.w, this.h);
+            context.restore();
+        } else {
+            context.save();
+            context.scale(1, 1);
+            context.drawImage(image, this.x, this.y, this.w, this.h);
+            context.restore();
+        }
     }
 }
 
 /*
  * Text class
  */
-function Text(x, y, value, w) {
+function Text(value, x, y, w) {
+    this.value = value || '';
     this.x = x || 100;
     this.y = y || 100;
-    this.value = value || '';
-    this.width = w || 400;
+    this.w = w || 0;
 }
 
 Text.prototype = {
+    drag: false,
+    h: 24,
+    fontSize: 30,
+    lineHeight: 32,
+
     _setInfo: function(x, y, w) {
-        this.x = x;
-        this.y = y;
-        this.width = w;
+        this.x = parseInt(x);
+        this.y = parseInt(y);
+        this.w = parseInt(w);
     },
 
-    _parseInt: function() {
-        this.x = parseInt(this.x);
-        this.y = parseInt(this.y);
-        this.width = parseInt(this.width);
+    _setFont: function(s, l) {
+        this.fontSize = parseInt(s);
+        this.lineHeight = parseInt(l);
     },
 
     wrapText: function(context, x, y, text, maxWidth, lineHeight) {
-        var words = text.split(' ');
-        var line = '';
-
-        for(var n = 0; n < words.length; n++) {
-            var testLine = line + words[n] + ' ';
-            var metrics = context.measureText(testLine);
-            var testWidth = metrics.width;
+        let words = text.split(' ');
+        let line = '';
+        for(let n = 0; n < words.length; n++) {
+            let testLine = line + words[n] + ' ';
+            let metrics = context.measureText(testLine);
+            let testWidth = metrics.width;
             if (testWidth > maxWidth && n > 0) {
                 context.fillText(line, x, y);
                 line = words[n] + ' ';
                 y += lineHeight;
-            }
-            else {
+                this.h = (y - this.y) + this.fontSize;
+            } else {
                 line = testLine;
             }
         }
@@ -98,10 +113,17 @@ Text.prototype = {
 
     _draw: function(canvas, context) {
         context.fillStyle = '#000';
-        context.textAlign = 'left';
-        context.font = `24px Arial`;
-        this._parseInt();
-        this.wrapText(context, this.x, this.y, this.value, this.width, 30);
+        context.font = `${this.fontSize}px Arial`;
+        context.textBaseline = 'hanging';
+        this.wrapText(context, this.x, this.y, this.value, this.w, this.lineHeight);
+        // context.beginPath();
+        // context.moveTo(this.x, this.y);
+        // context.lineTo(this.x + this.w, this.y);
+        // context.lineTo(this.x + this.w, this.y + this.h);
+        // context.lineTo(this.x, this.y + this.h);
+        // context.lineTo(this.x, this.y);
+        // context.strokeStyle = 'red';
+        // context.stroke();
     }
 }
 
@@ -132,16 +154,16 @@ Card.prototype = {
         return this.items;
     },
 
-    _setTitle: function(x, y, value, w) {
-        this.title = new Text(x, y, value, w);
+    _setTitle: function(value, x, y, w) {
+        this.title = new Text(value, x, y, w);
     },
 
     _getTitle: function() {
         return this.title;
     },
 
-    _setWish: function(x, y, value, w) {
-        this.wish = new Text(x, y, value, w);
+    _setWish: function(value, x, y, w) {
+        this.wish = new Text(value, x, y, w);
     },
 
     _getWish: function() {
@@ -151,11 +173,11 @@ Card.prototype = {
     _draw: function(canvas, context) {
         context.clearRect(0, 0, canvas.width, canvas.height);
         this.background._draw(context);
-        this.title._draw(canvas, context);
-        this.wish._draw(canvas, context);
         for(let i = 0; i < this.items.length; i++) {
             this.items[i]._draw(context);
         }
+        this.title._draw(canvas, context);
+        this.wish._draw(canvas, context);
     }
 }
 
@@ -198,8 +220,6 @@ Card.prototype = {
         messageY = this.children[0].dataset.messageY;
         messageWidth = this.children[0].dataset.messageWidth;
 
-        console.log(messageX);
-
         card.title._setInfo(titleX, titleY, titleWidth);
         card.wish._setInfo(messageX, messageY, messageWidth);
     }
@@ -238,28 +258,33 @@ Card.prototype = {
 
     function inputTitle() {
         let value = this.value ? this.value : '',
-            x = titleX || null,
-            y = titleY || null,
-            w = titleWidth || 500;
-        card._setTitle(x, y, value, w);
+            x = card._getTitle().x || titleX,
+            y = card._getTitle().y || titleY,
+            w = card._getTitle().w || titleWidth;
+        card._setTitle(value, x, y, w);
     }
 
     function inputWish() {
-        console.log(messageX);
+        // seperating title and message from the beginning
+        card._getWish().y = 300;
         let value = this.value ? this.value : '',
-            x = messageX || null,
-            y = messageY || null,
-            w = messageWidth || 500;
-        card._setWish(x, y, value, w);
+            x = card._getWish().x || messageX,
+            y = card._getWish().y || messageY,
+            w = card._getWish().w || messageWidth;
+        card._setWish(value, x, y, w);
     }
 
     const mouseMove = (e) => {
         let canvasView = canvas.getBoundingClientRect();
         Mouse.x = e.clientX - canvasView.left;
         Mouse.y = e.clientY - canvasView.top;
-        let items = card._getItems();
-        for(let i = items.length - 1; i >= 0; i--) {
-            let item = items[i],
+        let items = card._getItems(),
+            title = card._getTitle(),
+            wish = card._getWish();
+        let array = items.concat(title, wish);
+
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i],
                 isMouseOver = Mouse.x > item.x && Mouse.x < item.x + item.w && Mouse.y > item.y && Mouse.y < item.y + item.h;
 
             if(isMouseOver) {
@@ -270,8 +295,8 @@ Card.prototype = {
             }
         }
 
-        for(let i = items.length - 1; i >= 0; i--) {
-            let item = items[i];
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i];
             if(item.drag) {
                 item.x = Mouse.x - Mouse.dx;
                 item.y = Mouse.y - Mouse.dy;
@@ -281,9 +306,13 @@ Card.prototype = {
 
     const mouseDown = () => {
         Mouse.isDown = true;
-        let items = card._getItems();
-        for(let i = items.length - 1; i >= 0; i--) {
-            let item = items[i];
+        let items = card._getItems(),
+            title = card._getTitle(),
+            wish = card._getWish();
+        let array = items.concat(title, wish);
+
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i];
             if(
                 Mouse.x > item.x &&
                 Mouse.x < item.x + item.w &&
@@ -293,7 +322,6 @@ Card.prototype = {
                 item.drag = true;
                 Mouse.dx = Mouse.x - item.x;
                 Mouse.dy = Mouse.y - item.y;
-                // console.log(`item[${i}] is draggable.`);
                 return;
             }
         }
@@ -301,42 +329,61 @@ Card.prototype = {
 
     const mouseUp = () => {
         Mouse.isDown = false;
-        let items = card._getItems();
-        for(let i = 0; i < items.length; i++) {
-            let item = items[i];
+        let items = card._getItems(),
+            title = card._getTitle(),
+            wish = card._getWish();
+        let array = items.concat(title, wish);
+
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i];
             item.drag = false;
         }
     }
 
     const doubleClick = (e) => {
-        let items = card._getItems();
-        for(let i = items.length - 1; i >= 0; i--) {
-            let item = items[i];
+        let array = card._getItems();
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i];
             if(
                 Mouse.x > item.x &&
                 Mouse.x < item.x + item.w &&
                 Mouse.y > item.y &&
                 Mouse.y < item.y + item.h
             ) {
-                // console.log(`remove item[${i}].`);
-                items.splice(i ,1);
+                array.splice(i ,1);
                 return;
             }
         }
+    }
+
+    const contextmenu = (e) => {
+        e.preventDefault();
+        let array = card._getItems();
+        for(let i = array.length - 1; i >= 0; i--) {
+            let item = array[i];
+            if(
+                Mouse.x > item.x &&
+                Mouse.x < item.x + item.w &&
+                Mouse.y > item.y &&
+                Mouse.y < item.y + item.h
+            ) {
+                item.flip = !item.flip;
+                return;
+            }
+        }
+        return false;
     }
 
     /*
      * Event listeners
      */
 
-    backgroundEle.forEach(function(element, index) {
+    backgroundEle.forEach((element, index) => {
         backgroundEle[index].addEventListener('click', selectBackground, false);
     });
-
-    itemEle.forEach(function(element, index) {
+    itemEle.forEach((element, index) => {
         itemEle[index].addEventListener('click', selectItem, false);
     });
-
     titleEle.addEventListener('keyup', _debounce(inputTitle, 50, false), false);
     wishEle.addEventListener('keyup', _debounce(inputWish, 50, false), false);
 
@@ -344,6 +391,7 @@ Card.prototype = {
     canvas.addEventListener('mousedown', mouseDown, false);
     canvas.addEventListener('mouseup', mouseUp, false);
     canvas.addEventListener('dblclick', doubleClick, false);
+    canvas.addEventListener('contextmenu', contextmenu, false);
 
     // Update loop
     function update() {
