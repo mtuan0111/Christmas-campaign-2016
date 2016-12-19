83cde5887f1a68261c6bc44ca0e5aeb3f5a2b041
BOMNguyen <tuannguyen@vinaresearch.net>
2016-12-19 15:16:07 +0700

    [Update]

--- common/js/canvas.js
+++ common/js/canvas.js
@@ -1,3 +1,9 @@
+/******************************
+ * Date: Dec 14, 2015
+ * Canvas Controller
+ * author: Pham Quan
+ ******************************/
+
 'use-strict';
 
 window.requestAnimationFrame = window.requestAnimationFrame ||
@@ -9,9 +15,7 @@ window.requestAnimationFrame = window.requestAnimationFrame ||
                 window.setTimeout(callback, 1000 / 60);
             };
 
-let Mouse = {
-    x: 0, y: 0, dx: 0, dy: 0, isDown: false
-}
+let Mouse = { x: 0, y: 0, dx: 0, dy: 0, isDown: false }
 
 /*
  * Background class
@@ -37,8 +41,8 @@ Background.prototype = {
  */
 function Item(src, x, y, w, h) {
     this.src = src || '';
-    this.x = x || 0;
-    this.y = y || 0;
+    this.x = x;
+    this.y = y;
     this.w = w || 80;
     this.h = h || 80;
     this.naturalW = w || 80;
@@ -77,18 +81,21 @@ Item.prototype = {
 /*
  * Text class
  */
-function Text(value, x, y, w) {
+function Text(value, x, y, w, s, f, c) {
     this.value = value || '';
-    this.x = x || 100;
-    this.y = y || 100;
-    this.w = w || 0;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.fontSize = s || 30;
+    this.fontFamily = f || 'Arial';
+    this.color = c || '#000000';
+    this.lineHeight = this.fontSize * 1.2;
+    this.h = this.fontSize;
 }
 
 Text.prototype = {
     drag: false,
-    h: 24,
-    fontSize: 30,
-    lineHeight: 32,
+    isTouching: false,
 
     _setInfo: function(x, y, w) {
         this.x = parseInt(x);
@@ -96,9 +103,13 @@ Text.prototype = {
         this.w = parseInt(w);
     },
 
-    _setFont: function(s, l) {
-        this.fontSize = parseInt(s);
-        this.lineHeight = parseInt(l);
+    _setFont: function(s, f, w, c) {
+        this.fontSize = s;
+        this.fontFamily = f;
+        this.w = w;
+        this.h = this.fontSize;
+        this.color = c;
+        this.lineHeight = this.fontSize * 1.2;
     },
 
     wrapText: function(context, x, y, text, maxWidth, lineHeight) {
@@ -121,18 +132,19 @@ Text.prototype = {
     },
 
     _draw: function(canvas, context) {
-        context.fillStyle = '#000';
-        context.font = `${this.fontSize}px Arial`;
+        context.fillStyle = this.color;
+        context.font = `${this.fontSize}px ${this.fontFamily}`;
         context.textBaseline = 'hanging';
         this.wrapText(context, this.x, this.y, this.value, this.w, this.lineHeight);
-        // context.beginPath();
-        // context.moveTo(this.x, this.y);
-        // context.lineTo(this.x + this.w, this.y);
-        // context.lineTo(this.x + this.w, this.y + this.h);
-        // context.lineTo(this.x, this.y + this.h);
-        // context.lineTo(this.x, this.y);
-        // context.strokeStyle = 'red';
-        // context.stroke();
+
+        context.beginPath();
+        context.moveTo(this.x, this.y);
+        context.lineTo(this.x + this.w, this.y);
+        context.lineTo(this.x + this.w, this.y + this.h);
+        context.lineTo(this.x, this.y + this.h);
+        context.lineTo(this.x, this.y);
+        context.strokeStyle = 'black';
+        context.stroke();
     }
 }
 
@@ -163,16 +175,16 @@ Card.prototype = {
         return this.items;
     },
 
-    _setTitle: function(value, x, y, w) {
-        this.title = new Text(value, x, y, w);
+    _setTitle: function(value, x, y, w, f, c) {
+        this.title = new Text(value, x, y, w, f, c);
     },
 
     _getTitle: function() {
         return this.title;
     },
 
-    _setWish: function(value, x, y, w) {
-        this.wish = new Text(value, x, y, w);
+    _setWish: function(value, x, y, w, f, c) {
+        this.wish = new Text(value, x, y, w, f, c);
     },
 
     _getWish: function() {
@@ -181,12 +193,18 @@ Card.prototype = {
 
     _draw: function(canvas, context) {
         context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = '#ffffff';
+        context.fillRect(0, 0, canvas.width, canvas.height);
         this.background._draw(context);
         for(let i = 0; i < this.items.length; i++) {
             this.items[i]._draw(context);
         }
         this.title._draw(canvas, context);
         this.wish._draw(canvas, context);
+
+        let logo = new Image();
+        logo.src = 'common/img/canvas_logo.png';
+        context.drawImage(logo, 0, 0, 238, 87);
     }
 }
 
@@ -198,14 +216,11 @@ Card.prototype = {
 
     // Vars
     let canvas, context,
-        backgroundEle, itemEle, titleEle, wishEle, downloadBtn,
-        titleX = 100, titleY = 100, titleWidth = 500,
-        messageX = 100, messageY = 400, messageWidth = 500,
-        card, lastTouch,
+        backgroundEle, itemEle, titleEle, wishEle,
+        downloadBtn, textWidth, fontSize, fontFamily, reSize, color,
+        titleX = messageX = 100, titleY = 100, messageY = 400, titleWidth = messageWidth = 500,
+        card, lastItemTouched, lastTextTouched,
         items = [];
-
-
-    card = new Card();
 
     backgroundEle = document.querySelectorAll('#background-part li');
     itemEle = document.querySelectorAll('#object-part li');
@@ -213,42 +228,20 @@ Card.prototype = {
     wishEle = document.getElementById('card-content');
     downloadBtn = document.getElementById('download');
     reSize = document.getElementById('object-range');
+    fontSize = document.getElementById('font-size');
+    fontFamily = document.getElementById('font-family');
+    textWidth = document.getElementById('text-width');
+    color = document.getElementById('text-color');
 
     canvas = document.getElementById('canvas');
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     context = canvas.getContext('2d');
 
-    function selectBackground() {
-        let src = this.children[0].src;
-        card._setBackground(src, 0, 0, canvas.width, canvas.height);
+    // Create Card object
+    card = new Card();
 
-        // get info for Title + Wish
-        titleX = this.children[0].dataset.titleX;
-        titleY = this.children[0].dataset.titleY;
-        titleWidth = this.children[0].dataset.titleWidth;
-        messageX = this.children[0].dataset.messageX;
-        messageY = this.children[0].dataset.messageY;
-        messageWidth = this.children[0].dataset.messageWidth;
-
-        card.title._setInfo(titleX, titleY, titleWidth);
-        card.wish._setInfo(messageX, messageY, messageWidth);
-    }
-
-    function selectItem() {
-        let src = this.children[0].src,
-            w = this.children[0].naturalWidth,
-            h = this.children[0].naturalHeight,
-            max = 500,
-            min = 100,
-            x = Math.floor((Math.random() * (max - min +1)) + min),
-            y = Math.floor((Math.random() * (max - min +1)) + min);
-
-        reSize.value = 100;
-        card._setItems(src, x, y, w, h);
-        lastTouch = card._getItems().length - 1;
-    }
-
+    // Prevent event flooding
     function _debounce(fnc, wait, immediate) {
         let timeout;
         return function() {
@@ -269,29 +262,108 @@ Card.prototype = {
         }
     }
 
+    function selectBackground() {
+        let src = this.children[0].src;
+        card._setBackground(src, 0, 0, canvas.width, canvas.height);
+
+        // Get info for Title + Wish
+        titleX = this.children[0].dataset.titleX;
+        titleY = this.children[0].dataset.titleY;
+        titleWidth = this.children[0].dataset.titleWidth;
+        messageX = this.children[0].dataset.messageX;
+        messageY = this.children[0].dataset.messageY;
+        messageWidth = this.children[0].dataset.messageWidth;
+
+        card.title._setInfo(titleX, titleY, titleWidth);
+        card.wish._setInfo(messageX, messageY, messageWidth);
+
+        // Modify every infos after changing background
+        if(lastTextTouched == 0) {
+            textWidth.value = card._getTitle().w;
+            fontSize.value = card._getTitle().fontSize;
+            fontFamily.value = card._getTitle().fontFamily;
+            color.value = card._getTitle().color;
+        } else {
+            textWidth.value = card._getWish().w;
+            fontSize.value = card._getWish().fontSize;
+            fontFamily.value = card._getWish().fontFamily;
+            color.value = card._getWish().color;
+        }
+    }
+
+    function selectItem() {
+        let src = this.children[0].src,
+            w = this.children[0].naturalWidth,
+            h = this.children[0].naturalHeight,
+            max = 500,
+            min = 100,
+            x = Math.floor((Math.random() * (max - min +1)) + min),
+            y = Math.floor((Math.random() * (max - min +1)) + min);
+
+        card._setItems(src, x, y, w, h);
+        reSize.value = 100;
+        lastItemTouched = card._getItems().length - 1;
+    }
+
     function inputTitle() {
         let value = this.value ? this.value : '',
-            x = card._getTitle().x || titleX,
-            y = card._getTitle().y || titleY,
-            w = card._getTitle().w || titleWidth;
-        card._setTitle(value, x, y, w);
+            o = card._getTitle(),
+            x = o.x || titleX,
+            y = o.y || titleY,
+            w = o.w || titleWidth,
+            s = o.fontSize || parseInt(fontSize.value),
+            c = o.color || color.value,
+            f = o.fontFamily || fontFamily.value;
+
+        card._setTitle(value, x, y, w, s, f, c);
+        lastTextTouched = 0;
+        textWidth.value = o.w;
+        fontSize.value = o.fontSize;
+        fontFamily.value = o.fontFamily;
+        color.value = o.color;
     }
 
     function inputWish() {
-        // seperating title and message from the beginning
-        card._getWish().y = 300;
         let value = this.value ? this.value : '',
-            x = card._getWish().x || messageX,
-            y = card._getWish().y || messageY,
-            w = card._getWish().w || messageWidth;
-        card._setWish(value, x, y, w);
+            o = card._getWish(),
+            x = o.x || messageX,
+            y = o.y || messageY,
+            w = o.w || messageWidth,
+            s = o.fontSize || parseInt(fontSize.value),
+            c = o.color || color.value,
+            f = o.fontFamily || fontFamily.value;
+
+        card._setWish(value, x, y, w, s, f, c);
+        lastTextTouched = 1;
+        textWidth.value = o.w;
+        fontSize.value = o.fontSize;
+        fontFamily.value = o.fontFamily;
+        color.value = o.color;
     }
 
-    function changeSize() {
+    function changeItemSize() {
         let items = card._getItems();
-        items[lastTouch].ratio = parseInt(reSize.value);
-        console.log(lastTouch, items[lastTouch].ratio);
-        items[lastTouch]._setSize();
+        if(lastItemTouched != null) {
+            items[lastItemTouched].ratio = parseInt(reSize.value);
+            items[lastItemTouched]._setSize();
+        }
+    }
+
+    function changeText() {
+        let title = card._getTitle(),
+            wish = card._getWish(),
+            texts = [];
+
+        texts.push(title, wish);
+        if(lastTextTouched != null) {
+            let s = parseInt(fontSize.value),
+                w = parseInt(textWidth.value),
+                f = fontFamily.value.toString(),
+                c = color.value;
+
+            // Set: Font-size / Font-family / Width / Height / Line-height
+            texts[lastTextTouched]._setFont(s, f, w, c);
+        }
     }
 
     const mouseMove = (e) => {
@@ -303,18 +375,21 @@ Card.prototype = {
             wish = card._getWish();
         let array = items.concat(title, wish);
 
+        // Set cursor when mouse is over something
         for(let i = array.length - 1; i >= 0; i--) {
             let item = array[i],
                 isMouseOver = Mouse.x > item.x && Mouse.x < item.x + item.w && Mouse.y > item.y && Mouse.y < item.y + item.h;
 
             if(isMouseOver) {
-                canvas.style.cursor = 'pointer';
+                canvas.style.cursor = 'move';
                 break;
             } else {
                 canvas.style.cursor = 'default';
             }
         }
 
+        // if object is draggable
+        // Moving object along with Mouse
         for(let i = array.length - 1; i >= 0; i--) {
             let item = array[i];
             if(item.drag) {
@@ -326,11 +401,17 @@ Card.prototype = {
 
     const mouseDown = () => {
         Mouse.isDown = true;
+
         let items = card._getItems(),
             title = card._getTitle(),
-            wish = card._getWish();
-        let array = items.concat(title, wish);
+            wish = card._getWish(),
+            texts = [];
 
+        let array = items.concat(title, wish);
+        texts.push(title, wish);
+
+        // Items
+        // Input range control - item tab
         for(let i = items.length - 1; i >= 0; i--) {
             let item = items[i];
             if(
@@ -339,12 +420,33 @@ Card.prototype = {
                 Mouse.y > item.y &&
                 Mouse.y < item.y + item.h
             ) {
-                lastTouch = i;
+                lastItemTouched = i;
                 reSize.value = items[i].ratio;
                 break;
             }
         }
 
+        // Title + Wish
+        // Input range control - text tab
+        for(let i = texts.length - 1; i >= 0; i--) {
+            let text = texts[i];
+            if(
+                Mouse.x > text.x &&
+                Mouse.x < text.x + text.w &&
+                Mouse.y > text.y &&
+                Mouse.y < text.y + text.h
+            ) {
+                lastTextTouched = i;
+                textWidth.value = texts[i].w;
+                fontSize.value = texts[i].fontSize;
+                fontFamily.value = texts[i].fontFamily;
+                color.value = texts[i].color;
+                break;
+            }
+        }
+
+        // Items + Title + Wish
+        // Drag/drop control
         for(let i = array.length - 1; i >= 0; i--) {
             let item = array[i];
             if(
@@ -363,15 +465,26 @@ Card.prototype = {
 
     const mouseUp = () => {
         Mouse.isDown = false;
+
         let items = card._getItems(),
             title = card._getTitle(),
-            wish = card._getWish();
-        let array = items.concat(title, wish);
+            wish = card._getWish(),
+            texts = [];
 
+        let array = items.concat(title, wish);
+        texts.push(title, wish);
+
+        // Items
         for(let i = items.length - 1; i >= 0; i--) {
-            items[i].isResize = (i == lastTouch) ? true : false;
+            items[i].isResize = (i == lastItemTouched) ? true : false;
         }
 
+        // Title + Wish
+        for(let i = texts.length - 1; i >= 0; i--) {
+            texts[i].isTouching = (i == lastTextTouched) ? true : false;
+        }
+
+        // Items + Title + Wish
         for(let i = array.length - 1; i >= 0; i--) {
             let item = array[i];
             item.drag = false;
@@ -380,7 +493,6 @@ Card.prototype = {
 
     const doubleClick = (e) => {
         let array = card._getItems();
-        // lastTouch = null;
         for(let i = array.length - 1; i >= 0; i--) {
             let item = array[i];
             if(
@@ -413,9 +525,9 @@ Card.prototype = {
         return false;
     }
 
-    /*
+    /* --------------------------
      * Event listeners
-     */
+     * -------------------------- */
 
     backgroundEle.forEach((element, index) => {
         backgroundEle[index].addEventListener('click', selectBackground, false);
@@ -426,12 +538,42 @@ Card.prototype = {
     titleEle.addEventListener('keyup', _debounce(inputTitle, 50, false), false);
     wishEle.addEventListener('keyup', _debounce(inputWish, 50, false), false);
 
+    // all Actions on canvas
     canvas.addEventListener('mousemove', mouseMove, false);
     canvas.addEventListener('mousedown', mouseDown, false);
     canvas.addEventListener('mouseup', mouseUp, false);
     canvas.addEventListener('dblclick', doubleClick, false);
     canvas.addEventListener('contextmenu', contextmenu, false);
-    reSize.addEventListener('change', changeSize, false);
+
+    // Events that Interact with inputs
+    reSize.addEventListener('change', changeItemSize, false);
+    reSize.addEventListener('mousemove', changeItemSize, false);
+    fontSize.addEventListener('change', changeText, false);
+    fontSize.addEventListener('mousemove', changeText, false);
+    textWidth.addEventListener('change', changeText, false);
+    textWidth.addEventListener('mousemove', changeText, false);
+    color.addEventListener('change', changeText, false);
+    fontFamily.addEventListener('change', changeText, false);
+
+    // When focusing text area -> Show info of that text on input color/font-size/textbox width
+    titleEle.addEventListener('focus', function() {
+        let o = card._getTitle();
+        lastTextTouched = 0;
+        textWidth.value = o.w;
+        fontSize.value = o.fontSize;
+        fontFamily.value = o.fontFamily;
+        color.value = o.color;
+    }, false);
+    wishEle.addEventListener('focus', function() {
+        let o = card._getWish();
+        lastTextTouched = 1;
+        textWidth.value = o.w;
+        fontSize.value = o.fontSize;
+        fontFamily.value = o.fontFamily;
+        color.value = o.color;
+    }, false);
+
+    // Download button - Save image to local computer
     downloadBtn.addEventListener('click', function() {
         canvas.toBlob(function(blob) {
             saveAs(blob, 'christmas-card.png')
@@ -439,7 +581,8 @@ Card.prototype = {
     }, false);
 
 
-    // Update loop
+    // Update loop to re-drawn
+    // Guarantee 60fps
     function update() {
         card._draw(canvas, context);
         requestAnimationFrame(update);
